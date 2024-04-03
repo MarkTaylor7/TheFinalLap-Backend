09324bcfc7068d13c59ac8d7ad569851b0fe4fb0
Mark Taylor <mtaylor_001@hotmail.com>
2024-04-03 20:59:28 -0230

    Fixed lint error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { type Request, type Response } from 'express'
 import db from './database'
+import { asyncHandler } from './utils/async-handler'
 
 const app = express()
 const port = 3000
 
-app.get('/users', async (req: Request, res: git pullResponse) => {
+app.get('/users', asyncHandler(async (req: Request, res: Response) => {
   try {
     const [rows] = await db.execute('SELECT * FROM users')
     res.json(rows)
@@ -12,7 +13,7 @@ app.get('/users', async (req: Request, res: git pullResponse) => {
     console.error('Error fetching users:', error)
     res.status(500).send('An error occurred')
   }
-})
+}))
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`)
