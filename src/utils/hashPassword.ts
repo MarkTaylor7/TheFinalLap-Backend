425656e527d7a958bc98ac6248b06dd827f1c7c9
Mark Taylor <mtaylor_001@hotmail.com>
2024-04-26 11:58:33 -0230

    Corrected formatting

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,24 +1,24 @@
-import * as crypto from 'crypto';
+import * as crypto from 'crypto'
 
-const SALT_LENGTH = 16;
-const ITERATIONS = 100000;
-const KEY_LENGTH = 64;
+const SALT_LENGTH = 16
+const ITERATIONS = 100000
+const KEY_LENGTH = 64
 
 export interface password {
   salt: string
   hash: string
 }
 
-export function hashPassword(password: string): password {
-  const salt = crypto.randomBytes(SALT_LENGTH).toString('hex');
-  const hash = crypto.pbkdf2Sync(password, salt, ITERATIONS, KEY_LENGTH, 'sha512');
+export function hashPassword (password: string): password {
+  const salt = crypto.randomBytes(SALT_LENGTH).toString('hex')
+  const hash = crypto.pbkdf2Sync(password, salt, ITERATIONS, KEY_LENGTH, 'sha512')
   return {
     salt,
     hash: hash.toString('hex')
-  };
+  }
 }
 
-export function verifyPassword(password: string, salt: string, hash: string) {
+export function verifyPassword (password: string, salt: string, hash: string) {
   const verifyHash = crypto.pbkdf2Sync(password, salt, ITERATIONS, KEY_LENGTH, 'sha512').toString('hex');
   return hash === verifyHash;
 }
