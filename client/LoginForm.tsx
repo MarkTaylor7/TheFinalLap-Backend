38c3260dd6cca05d51104bbadd744db178abeecc
Mark Taylor <mtaylor_001@hotmail.com>
2024-05-14 22:25:43 -0230

    Setup React client and login login form

---
+++ client/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const LoginForm: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    alert(data.message);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
